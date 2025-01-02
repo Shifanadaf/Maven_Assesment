5c86954ce825daaaabb75cf238bfd953a2234092
Shifa Nadaf <125539242+Shifanadaf@users.noreply.github.com>
2025-01-02 19:22:00 +0530

    Update Fibonacci.java

--- src/main/java/com/example/maven_assesment/Fibonacci.java
+++ src/main/java/com/example/maven_assesment/Fibonacci.java
@@ -10,12 +10,22 @@ import java.util.List;
         
         if (nterms <= 0) {
             throw new IllegalArgumentException("Please enter a positive integer");
-        } else if (nterms == 1) {
+        }
+        if (nterms <= 0) {
+            throw new IllegalArgumentException("Please enter a positive integer");
+        }
+        else if (nterms == 1) {
             sequence.add(0);
         } else {
             int n1 = 0;
            int n2 = 1;
             for (int count = 0; count < nterms; count++) {
+                sequence.add(n1);
+                int nth = n1 + n2;
+                n1 = n2;
+                n2 = nth;
+            }
+          for (int count = 0; count < nterms; count++) {
                 sequence.add(n1);
                 int nth = n1 + n2;
                 n1 = n2;
